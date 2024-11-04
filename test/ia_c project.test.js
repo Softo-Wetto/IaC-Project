7b63256f06bc7bcf671b4aec9fcf131747a3fd1b
Softo-Wetto <nightmareasian@gmail.com>
2024-11-05 04:42:55 +1000

    Initial commit

---
+++ test/ia_c project.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const IaCProject = require('../lib/ia_c project-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ia_c project-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new IaCProject.IaCProjectStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
